7b9bcdeeb3083008fbb656c4c39d3252eb000b2a
Marcio Junior <marciofmjr@hotmail.com>
2017-08-09 11:57:38 -0300

    Cria config para telefones no admin

---
+++ Helper/Data.php
@@ -0,0 +1,27 @@
+<?php
+class Cammino_Themeconfig_Helper_Data extends Mage_Core_Helper_Abstract
+{
+	public function getPhone($type, $indice){
+		$phone = Mage::getStoreConfig('themeconfig/themeconfig_group_phones/themeconfig_'.$type.'_'.$indice);
+		if($phone != null && strlen($phone) > 4){
+			return array(
+				'number' 	=> $phone,
+				'type'		=> $type,
+				'indice'	=> $indice
+			);
+		}else{
+			return false;
+		}
+	}
+
+	public function getTelephones(){
+		$telephones = array();
+		if($this->getPhone("telephone", "1")): $telephones[] = $this->getPhone("telephone", "1"); endif;
+		if($this->getPhone("telephone", "2")): $telephones[] = $this->getPhone("telephone", "2"); endif;
+		if($this->getPhone("cellphone", "1")): $telephones[] = $this->getPhone("cellphone", "1"); endif;
+		if($this->getPhone("cellphone", "2")): $telephones[] = $this->getPhone("cellphone", "2"); endif;
+		if($this->getPhone("whatsapp", "1")): $telephones[] = $this->getPhone("whatsapp", "1"); endif;
+		if($this->getPhone("whatsapp", "2")): $telephones[] = $this->getPhone("whatsapp", "2"); endif;
+		return $telephones;
+	}
+}
