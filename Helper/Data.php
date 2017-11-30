1b1fa875fd96a4818b0f6e762c83a3c21e11a947
Marcio Junior <marciofmjr@hotmail.com>
2017-11-30 09:10:21 -0200

    Cria opções para setar link das redes sociais pelas configs do tema no admin

--- Helper/Data.php
+++ Helper/Data.php
@@ -1,6 +1,7 @@
 <?php
 class Cammino_Themeconfig_Helper_Data extends Mage_Core_Helper_Abstract
 {
+
 	public function getProductAttributesTab(){
 		$attributes = Mage::getStoreConfig('themeconfig/themeconfig_group_product/themeconfig_attributes_tab');
 		if($attributes != null){
@@ -51,5 +52,66 @@ class Cammino_Themeconfig_Helper_Data extends Mage_Core_Helper_Abstract
 		if($this->getMail("1")): $emails[] = $this->getMail("1"); endif;
 		if($this->getMail("2")): $emails[] = $this->getMail("2"); endif;
 		return $emails;
+	}
+
+	public function getSocialMedias(){
+		$medias = array();
+		if($this->hasFacebook()): $medias[] = $this->getFacebook(); endif;
+		if($this->hasInstagram()): $medias[] = $this->getInstagram(); endif;
+		if($this->hasTwitter()): $medias[] = $this->getTwitter(); endif;
+		if($this->hasYoutube()): $medias[] = $this->getYoutube(); endif;
+		return $medias;
+	}
+
+	public function getFacebook(){
+		return array(
+			"url" => Mage::getStoreConfig('themeconfig/themeconfig_group_social_media/facebook'),
+			"slug" => "facebook",
+			"label" => "Facebook",
+		);
+	}
+
+	public function getInstagram(){
+		return array(
+			"url" => Mage::getStoreConfig('themeconfig/themeconfig_group_social_media/instagram'),
+			"slug" => "instagram",
+			"label" => "Instagram",
+		);
+	}
+
+	public function getTwitter(){
+		return array(
+			"url" => Mage::getStoreConfig('themeconfig/themeconfig_group_social_media/twitter'),
+			"slug" => "twitter",
+			"label" => "Twitter",
+		);
+	}
+
+	public function getYoutube(){
+		return array(
+			"url" => Mage::getStoreConfig('themeconfig/themeconfig_group_social_media/youtube'),
+			"slug" => "youtube",
+			"label" => "Youtube",
+		);
+	}
+
+	public function hasFacebook(){
+		$facebook = Mage::getStoreConfig('themeconfig/themeconfig_group_social_media/facebook');
+		return strlen($facebook) > 3;
+	}
+
+	public function hasInstagram(){
+		$instagram = Mage::getStoreConfig('themeconfig/themeconfig_group_social_media/instagram');
+		return strlen($instagram) > 3;
+	}
+
+	public function hasTwitter(){
+		$twitter = Mage::getStoreConfig('themeconfig/themeconfig_group_social_media/twitter');
+		return strlen($twitter) > 3;
+	}
+
+	public function hasYoutube(){
+		$youtube = Mage::getStoreConfig('themeconfig/themeconfig_group_social_media/youtube');
+		return strlen($youtube) > 3;
 	}
 }
