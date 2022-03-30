c440da986dc66a0afea13616a32f79287538f3b5
ArthurMenegoli <arthur.menegoli@gmail.com>
2022-03-30 05:02:09 -0300

    #32747 - Add PIX to alternate price listing

--- Model/Source/Design/Theme/Alternativeprice.php
+++ Model/Source/Design/Theme/Alternativeprice.php
@@ -6,8 +6,12 @@ class Cammino_Themeconfig_Model_Source_Design_Theme_Alternativeprice
     {
 			return array (
 				array('value' => 'installments', 'label' => 'Parcelamento'),
+				array('value' => 'installments_billet', 'label' => 'Parcelamento e preço no boleto'),
+				array('value' => 'installments_pix', 'label' => 'Parcelamento e preço no pix'),
 				array('value' => 'billet', 'label' => 'Preço no boleto'),
-				array('value' => 'both', 'label' => 'Ambos'),
+				array('value' => 'billet_pix', 'label' => 'Preço no boleto e no PIX'),
+				array('value' => 'pix', 'label' => 'Preço no PIX'),
+				array('value' => 'all', 'label' => 'Todos'),
 				array('value' => 'none', 'label' => 'Nenhum'),
 			);
 	}
